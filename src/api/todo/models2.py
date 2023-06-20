f37b7bcb9117ebc42efebeeccaba428ec4a331d7
vmagelo <51368918+vmagelo@users.noreply.github.com>
2023-06-20 17:48:53 +0000

    Convert api to sql model.

--- src/api/todo/models2.py
+++ src/api/todo/models2.py
@@ -7,10 +7,37 @@ from beanie import PydanticObjectId
 
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
-from pydantic import BaseSettings
+from pydantic import BaseSettings, BaseModel
 
 def keyvault_name_as_attr(name: str) -> str:
     return name.replace("-", "_").upper()
+
+class Settings2(BaseSettings):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Load secrets from keyvault
+        if self.AZURE_KEY_VAULT_ENDPOINT:
+            credential = DefaultAzureCredential()
+            keyvault_client = SecretClient(self.AZURE_KEY_VAULT_ENDPOINT, credential)
+            for secret in keyvault_client.list_properties_of_secrets():
+                setattr(
+                    self,
+                    keyvault_name_as_attr(secret.name),
+                    keyvault_client.get_secret(secret.name).value,
+                )
+
+    AZURE_POSTGRESQL_CONNECTION_STRING: str = ""
+    AZURE_POSTGRESQL_DATABASE_NAME: str = "Todo"
+    AZURE_COSMOS_CONNECTION_STRING: str = ""
+    AZURE_COSMOS_DATABASE_NAME: str = "Todo"
+    AZURE_KEY_VAULT_ENDPOINT: Optional[str] = None
+    APPLICATIONINSIGHTS_CONNECTION_STRING: Optional[str] = None
+    APPLICATIONINSIGHTS_ROLENAME: Optional[str] = "API"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 class TodoList2(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -19,7 +46,7 @@ class TodoList2(SQLModel, table=True):
     createdDate: Optional[datetime] = Field(default=None)
     updatedDate: Optional[datetime] = Field(default=None)
 
-class CreateUpdateTodoList2(SQLModel):
+class CreateUpdateTodoList2(BaseModel):
     name: str
     description: Optional[str] = None
 
@@ -39,7 +66,7 @@ class TodoItem2(SQLModel, table=True):
     createdDate: Optional[datetime] = None
     updatedDate: Optional[datetime] = None
 
-class CreateUpdateTodoItem2(SQLModel):
+class CreateUpdateTodoItem2(BaseModel):
     name: str
     description: Optional[str] = None
     state: Optional[TodoState2] = None
--- src/api/todo/routes.py
+++ src/api/todo/routes.py
@@ -14,7 +14,7 @@ from .models import (CreateUpdateTodoItem, CreateUpdateTodoList, TodoItem,
 from .models2 import (CreateUpdateTodoItem2, CreateUpdateTodoList2, TodoItem2,
                         TodoList2, TodoState2)
 
-@app.get("/lists2", response_model=List[TodoList2], response_model_by_alias=False)
+@app.get("/lists", response_model=List[TodoList2], response_model_by_alias=False)
 async def get_lists2(
     top: Optional[int] = None, skip: Optional[int] = None
 ) -> List[TodoList2]:
@@ -31,7 +31,7 @@ async def get_lists2(
         return query.all()
 
 
-@app.get("/lists", response_model=List[TodoList], response_model_by_alias=False)
+@app.get("/oldlists", response_model=List[TodoList], response_model_by_alias=False)
 async def get_lists(
     top: Optional[int] = None, skip: Optional[int] = None
 ) -> List[TodoList]:
@@ -47,20 +47,19 @@ async def get_lists(
     return await query.to_list()
 
 
-@app.post("/lists2", response_model=TodoList2, response_model_by_alias=False, status_code=201)
+@app.post("/lists", response_model=TodoList2, response_model_by_alias=False, status_code=201)
 async def create_list2(body: CreateUpdateTodoList2, request: Request, response: Response) -> TodoList2:
     """
     Create a new Todo list
     """
-    todo_list2 = TodoList2(**body.dict(), createdDate=datetime.utcnow())
-    session = Session(engine)
-    session.add(todo_list2)
-    session.commit()
-    response.headers["Location"] = urljoin(str(request.base_url), "lists/{0}".format(str(todo_list2.id)))
-    session.close()
-    return todo_list2
+    with Session(engine) as session:
+        todo_list2 = TodoList2(**body.dict(), createdDate=datetime.utcnow(), updatedDate=datetime.utcnow())
+        session.add(todo_list2)
+        session.commit()
+        response.headers["Location"] = urljoin(str(request.base_url), "lists/{0}".format(str(todo_list2.id)))
+        return todo_list2
 
-@app.post("/lists", response_model=TodoList, response_model_by_alias=False, status_code=201)
+@app.post("/oldlists", response_model=TodoList, response_model_by_alias=False, status_code=201)
 async def create_list(body: CreateUpdateTodoList, request: Request, response: Response) -> TodoList:
     """
     Create a new Todo list
@@ -69,8 +68,19 @@ async def create_list(body: CreateUpdateTodoList, request: Request, response: Re
     response.headers["Location"] = urljoin(str(request.base_url), "lists/{0}".format(str(todo_list.id)))
     return todo_list
 
+@app.get("/lists/{list_id}", response_model=TodoList2, response_model_by_alias=False)
+async def get_list2(list_id: int) -> TodoList2:
+    """
+    Get Todo list by ID
+    """
+    with Session(engine) as session:
+        query = session.exec(select(TodoList2).where(TodoList2.id == list_id))
+        todo_list2 = query.first()
+        if not todo_list2:
+            raise HTTPException(status_code=404, detail="Todo list not found")
+        return todo_list2
 
-@app.get("/lists/{list_id}", response_model=TodoList, response_model_by_alias=False)
+@app.get("/oldlists/{list_id}", response_model=TodoList, response_model_by_alias=False)
 async def get_list(list_id: PydanticObjectId) -> TodoList:
     """
     Get Todo list by ID
@@ -80,8 +90,27 @@ async def get_list(list_id: PydanticObjectId) -> TodoList:
         raise HTTPException(status_code=404, detail="Todo list not found")
     return todo_list
 
+@app.put("/lists/{list_id}", response_model=TodoList2, response_model_by_alias=False)
+async def update_list2(
+    list_id: int, body: CreateUpdateTodoList2
+) -> TodoList2:
+    """
+    Updates a Todo list by unique identifier
+    """
+    with Session(engine) as session:
+        query = session.exec(select(TodoList2).where(TodoList2.id == list_id))
+        todo_list2 = query.one()
+        if not todo_list2:
+            raise HTTPException(status_code=404, detail="Todo list not found")
+        todo_list2.name = body.name
+        todo_list2.description = body.description
+        todo_list2.updatedDate = datetime.utcnow()
+        session.add(todo_list2)
+        session.commit()
+        session.refresh(todo_list2)
+        return todo_list2
 
-@app.put("/lists/{list_id}", response_model=TodoList, response_model_by_alias=False)
+@app.put("/oldlists/{list_id}", response_model=TodoList, response_model_by_alias=False)
 async def update_list(
     list_id: PydanticObjectId, body: CreateUpdateTodoList
 ) -> TodoList:
@@ -95,8 +124,20 @@ async def update_list(
     todo_list.updatedDate = datetime.utcnow()
     return await todo_list.save()
 
-
 @app.delete("/lists/{list_id}", response_class=Response, status_code=204)
+async def delete_list2(list_id: int) -> None:
+    """
+    Deletes a Todo list by unique identifier
+    """
+    with Session(engine) as session:
+        query = session.exec(select(TodoList2).where(TodoList2.id == list_id))
+        todo_list2 = query.first()
+        if not todo_list2:
+            raise HTTPException(status_code=404, detail="Todo list not found")
+        session.delete(todo_list2)
+        session.commit()
+
+@app.delete("/oldlists/{list_id}", response_class=Response, status_code=204)
 async def delete_list(list_id: PydanticObjectId) -> None:
     """
     Deletes a Todo list by unique identifier
@@ -106,8 +147,21 @@ async def delete_list(list_id: PydanticObjectId) -> None:
         raise HTTPException(status_code=404, detail="Todo list not found")
     await todo_list.delete()
 
+@app.post("/lists/{list_id}/items", response_model=TodoItem2, response_model_by_alias=False, status_code=201)
+async def create_list_item2(
+    list_id: int, body: CreateUpdateTodoItem2, request: Request, response: Response
+) -> TodoItem2:
+    """
+    Creates a new Todo item within a list
+    """
+    with Session(engine) as session:
+        todo_item2 = TodoItem2(listId=list_id, **body.dict(), createdDate=datetime.utcnow())
+        session.add(todo_item2)
+        session.commit()
+        response.headers["Location"] = urljoin(str(request.base_url), "lists/{0}/items/{1}".format(str(list_id), str(todo_item2.id)))
+        return todo_item2
 
-@app.post("/lists/{list_id}/items", response_model=TodoItem, response_model_by_alias=False, status_code=201)
+@app.post("/oldlists/{list_id}/items", response_model=TodoItem, response_model_by_alias=False, status_code=201)
 async def create_list_item(
     list_id: PydanticObjectId, body: CreateUpdateTodoItem, request: Request, response: Response
 ) -> TodoItem:
@@ -118,8 +172,24 @@ async def create_list_item(
     response.headers["Location"] = urljoin(str(request.base_url), "lists/{0}/items/{1}".format(str(list_id), str(item.id)))
     return await item.save()
 
+@app.get("/lists/{list_id}/items", response_model=List[TodoItem2], response_model_by_alias=False)
+async def get_list_items2(
+    list_id: int,
+    top: Optional[int] = None,
+    skip: Optional[int] = None,
+) -> List[TodoItem2]:
+    """
+    Gets Todo items within a specified list
 
-@app.get("/lists/{list_id}/items", response_model=List[TodoItem], response_model_by_alias=False)
+    - **top**: Number of lists to return
+    - **skip**: Number of lists to skip
+    """    
+    with Session(engine) as session:
+        query = session.exec(select(TodoItem2).where(TodoItem2.listId == str(list_id)).offset(skip).limit(top))
+        return query.all()
+
+
+@app.get("/oldlists/{list_id}/items", response_model=List[TodoItem], response_model_by_alias=False)
 async def get_list_items(
     list_id: PydanticObjectId,
     top: Optional[int] = None,
@@ -137,7 +207,33 @@ async def get_list_items(
     return await query.to_list()
 
 
-@app.get("/lists/{list_id}/items/state/{state}", response_model=List[TodoItem], response_model_by_alias=False)
+
+@app.get("/lists/{list_id}/items/state/{state}", response_model=List[TodoItem2], response_model_by_alias=False)
+async def get_list_items_by_state2(
+    list_id: int,
+    state: TodoState2 = ...,
+    top: Optional[int] = None,
+    skip: Optional[int] = None,
+) -> List[TodoItem2]:
+    """
+    Gets a list of Todo items of a specific state
+
+    Optional arguments:
+
+    - **top**: Number of lists to return
+    - **skip**: Number of lists to skip
+    """    
+    with Session(engine) as session:
+        query = (
+            session.exec(
+                select(TodoItem2).where(TodoItem2.listId == str(list_id), TodoItem2.state == state.name)
+                    .offset(skip)
+                    .limit(top)
+            )
+        )
+        return query.all()
+
+@app.get("/oldlists/{list_id}/items/state/{state}", response_model=List[TodoItem], response_model_by_alias=False)
 async def get_list_items_by_state(
     list_id: PydanticObjectId,
     state: TodoState = ...,
@@ -160,7 +256,32 @@ async def get_list_items_by_state(
     return await query.to_list()
 
 
-@app.put("/lists/{list_id}/items/state/{state}", response_model=List[TodoItem], response_model_by_alias=False)
+@app.put("/lists/{list_id}/items/state/{state}", response_model=List[TodoItem2], response_model_by_alias=False)
+async def update_list_items_state2(
+    list_id: int,
+    state: TodoState2 = ...,
+    body: List[int] = None,
+) -> List[TodoItem2]:
+    """
+    Changes the state of the specified list items
+    """
+    if not body:
+        raise HTTPException(status_code=400, detail="No items specified")
+    results = []
+    for id_ in body:
+        with Session(engine) as session:
+            item = session.exec(select(TodoItem2).where(TodoItem2.id == id_)).first()
+            if not item:
+                raise HTTPException(status_code=404, detail="Todo item not found")
+            item.state = state
+            item.updatedDate = datetime.utcnow()
+            session.add(item)
+            session.commit()
+            session.refresh(item)
+            results.append(item)
+    return results
+
+@app.put("/oldlists/{list_id}/items/state/{state}", response_model=List[TodoItem], response_model_by_alias=False)
 async def update_list_items_state(
     list_id: PydanticObjectId,
     state: TodoState = ...,
@@ -182,7 +303,23 @@ async def update_list_items_state(
     return results
 
 
-@app.get("/lists/{list_id}/items/{item_id}", response_model=TodoItem, response_model_by_alias=False)
+@app.get("/lists/{list_id}/items/{item_id}", response_model=TodoItem2, response_model_by_alias=False)
+async def get_list_item2(
+    list_id: int, 
+    item_id: int
+) -> TodoItem2:
+    """
+    Gets a Todo item by unique identifier
+    """
+    with Session(engine) as session:
+        item = session.exec(
+            select(TodoItem2).where(TodoItem2.listId == str(list_id), TodoItem2.id == item_id)
+        ).one()
+        if not item:
+            raise HTTPException(status_code=404, detail="Todo item not found")
+        return item
+
+@app.get("/oldlists/{list_id}/items/{item_id}", response_model=TodoItem, response_model_by_alias=False)
 async def get_list_item(
     list_id: PydanticObjectId, item_id: PydanticObjectId
 ) -> TodoItem:
@@ -195,7 +332,33 @@ async def get_list_item(
     return item
 
 
-@app.put("/lists/{list_id}/items/{item_id}", response_model=TodoItem, response_model_by_alias=False)
+@app.put("/lists/{list_id}/items/{item_id}", response_model=TodoItem2, response_model_by_alias=False)
+async def update_list_item2(
+    list_id: int,
+    item_id: int,
+    body: CreateUpdateTodoItem2,
+) -> TodoItem2:
+    """
+    Updates a Todo item by unique identifier
+    """
+    with Session(engine) as session:
+        item = session.exec(
+            select(TodoItem2).where(TodoItem2.listId == str(list_id), TodoItem2.id == item_id)
+        ).one()
+        if not item:
+            raise HTTPException(status_code=404, detail="Todo item not found")
+        if body.name:
+            item.name = body.name
+        if body.description:
+            item.description = body.description
+        if body.state:
+            item.state = body.state
+        item.updatedDate = datetime.utcnow()
+        session.commit()
+        session.refresh(item)
+        return item
+
+@app.put("/oldlists/{list_id}/items/{item_id}", response_model=TodoItem, response_model_by_alias=False)
 async def update_list_item(
     list_id: PydanticObjectId,
     item_id: PydanticObjectId,
@@ -211,8 +374,24 @@ async def update_list_item(
     item.updatedDate = datetime.utcnow()
     return await item.save()
 
-
 @app.delete("/lists/{list_id}/items/{item_id}", response_class=Response, status_code=204)
+async def delete_list_item2(
+    list_id: int, item_id: int
+) -> None:
+    """
+    Deletes a Todo item by unique identifier
+    """
+    try:
+        with Session(engine) as session:
+            item = session.exec(
+                select(TodoItem2).where(TodoItem2.listId == str(list_id), TodoItem2.id == item_id)
+            ).one()
+            session.delete(item)
+            session.commit()
+    except:            
+        raise HTTPException(status_code=404, detail="Todo item not found")
+
+@app.delete("/oldlists/{list_id}/items/{item_id}", response_class=Response, status_code=204)
 async def delete_list_item(
     list_id: PydanticObjectId, item_id: PydanticObjectId
 ) -> None:
